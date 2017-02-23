f49d0f0ebdb0cbbf800fa221f2a1da0c853044bc
Scott Travis <scott@swt83.com>
2017-02-23 13:53:56 -0600

    Delete start.php

--- start.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-#################################################
-# Legacy autoloader for Laravel 3.
-#################################################
-Autoloader::map(array(
-    'CSV' => __DIR__.'/src/Travis/CSV.php',
-));
